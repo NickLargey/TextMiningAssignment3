8c5c99d2e4d61ed9c9083dcf75c618ab8b317184
___GNUk <Nick.Largey@gmail.com>
2024-03-19 14:55:36 -0400

    actual FIXED csv

--- RNN.py
+++ RNN.py
@@ -2,11 +2,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import spacy
+from spacy import displacy
+
+
+def EDA(dataframe):
+  df = pd.copy(dataframe)
+
+  nlp = spacy.load("en_core_web_sm")
+  text = df.iloc[0]['lyrics']
+  doc = nlp(text)
+  displacy.serve(doc, style="dep")
+
+
+
 
 
 def main():
   df = pd.read_csv('./lyrics.csv')
-  print(df.head())    
+  EDA(df)   
 
 if __name__ == '__main__':
   main()
